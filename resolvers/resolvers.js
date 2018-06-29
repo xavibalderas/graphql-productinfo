2c8df0c9f38083731c77e2caf80d7878bb0db2d0
Francisco Javier Balderas Cejudo <frbal5@retch078-xw0004.ikea.com>
2018-06-29 16:48:35 +0200

    Added product resolver

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,12 +1,29 @@
-const { request } = require('graphql-request');
+const { gqlRequest } = require('graphql-request');
 const { queries } = require('../queries/queries.js');
+//const { request } = require ('request');
+const request = require('request-promise');
+const { parseString } = require('xml2js');
+const fastXmlParser = require('fast-xml-parser');
+//const rp = require('request-promise');
 
 const URI_API = process.env.GRAPHCMS_API;
+
+const optionsR = {
+    uri: 'https://www.ikea.com/ch/de/catalog/products/80404672?type=xml',
+    json: false // Automatically parses the JSON string in the response
+};
+
+request(optionsR).then(body => {
+  console.log('34234');
+  const parsedXML = fastXmlParser.parse(body);
+  console.log(parsedXML);
+  console.log("fdsf");
+});
 
 const resolvers = {
   Query: {
     allCombinations: () => {
-      return request(URI_API, queries.allbeds).then(data => {console.log(data); return data.allBeds;})
+      return gqlRequest(URI_API, queries.allbeds).then(data => {console.log(data); return data.allBeds;})
     },
   },
 };
