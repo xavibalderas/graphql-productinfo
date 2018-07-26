99d81a9af2054921633a9664a78e4f57645ddfbc
Francisco Javier Balderas Cejudo <frbal5@retch078-xw0004.ikea.com>
2018-07-26 17:45:18 +0200

    Solver a problem with product without family price

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -19,17 +19,23 @@ const resolvers = {
         console.log("----FINISH_RAW_DATA----");
         return xmlPromise.parse(response.data).then(data => {
           console.log(data);
-
+console.log(data['ir:ikea-rest'].products.product.items.item.prices['family-normal']);
           var _d = {
             name: data['ir:ikea-rest'].products.product.name,
-            partNumber: data['ir:ikea-rest'].products.product.partNumber,
+            partNumber: data['ir:ikea-rest'].products.product.items.item.partNumber,
             type: data['ir:ikea-rest'].products.product.items.item.type,
             normalPrice: data['ir:ikea-rest'].products.product.items.item.prices.normal.priceNormal.attr.unformatted,
             secondPrice: data['ir:ikea-rest'].products.product.items.item.prices.second.priceNormal,
-            familyPrice_startDate: data['ir:ikea-rest'].products.product.items.item.prices['family-normal'].priceNormal.attr.startDate,
-            familyPrice_endDate: data['ir:ikea-rest'].products.product.items.item.prices['family-normal'].priceNormal.attr.endDate,
-            familyPrice_price: data['ir:ikea-rest'].products.product.items.item.prices['family-normal'].priceNormal.attr.unformatted,
-            familyPrice_disclaimer: data['ir:ikea-rest'].products.product.items.item.prices['family-normal'].priceDisclaimer,
+            familyPrice_startDate: '',
+            familyPrice_endDate: '',
+            familyPrice_price: '',
+            familyPrice_disclaimer: ''
+          }
+          if(data['ir:ikea-rest'].products.product.items.item.prices['family-normal'].priceNormal!=''){
+            _d.familyPrice_startDate = data['ir:ikea-rest'].products.product.items.item.prices['family-normal'].priceNormal.attr.startDate;
+            _d.familyPrice_endDate = data['ir:ikea-rest'].products.product.items.item.prices['family-normal'].priceNormal.attr.endDate;
+            _d.familyPrice_price = data['ir:ikea-rest'].products.product.items.item.prices['family-normal'].priceNormal.attr.unformatted;
+            _d.familyPrice_disclaimer = data['ir:ikea-rest'].products.product.items.item.prices['family-normal'].priceDisclaimer;
           }
           return _d;
         })
